80e5995ba95666a6738bac7891f208da74843e98
Jayson Virissimo <jayson.virissimo@asu.edu>
2015-12-08 13:02:03 -0700

    Update deprecated RSpec syntax

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -4,15 +4,15 @@ describe "index", :type => :feature do
   before do
     visit "/"
   end
-  
+
   it "has the correct title header" do
-    page.should have_selector "h1"
+    expect(page).to have_selector "h1"
     within "h1" do
-      page.should have_content /Middleman is Watching/i
+      expect(page).to have_content /Middleman is Watching/i
     end
   end
-  
+
   it "has a paragraph in there too" do
-    page.should have_selector "p.doc"
+    expect(page).to have_selector "p.doc"
   end
 end
