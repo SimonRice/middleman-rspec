5d05bbf9c8e9853e441099017036062bd6172edb
Joshua Steele <joshukraine@gmail.com>
2016-10-13 21:30:14 +0300

    Move settings block to Middleman app initialization.
    * Settings block was previously under Capybara.app.
    * Environment settings in particular (and likely the other two) had no
    affect there.
    * By placing the block in the Middleman app initialization, settings
    actually have affect.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,10 @@ require 'capybara/rspec'
 require 'middleman-core'
 require 'middleman-core/rack'
 
-middleman_app = ::Middleman::Application.new
-
-Capybara.app = ::Middleman::Rack.new(middleman_app).to_app do
+middleman_app = ::Middleman::Application.new do
   set :root, File.expand_path(File.join(File.dirname(__FILE__), '..'))
   set :environment, :development
   set :show_exceptions, false
 end
+
+Capybara.app = ::Middleman::Rack.new(middleman_app).to_app
